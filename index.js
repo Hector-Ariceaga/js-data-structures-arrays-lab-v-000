fe85b84261250745f5138f3e173b0c5fe9af140b
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-06 18:48:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ const drivers = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendDriver(name) {
   drivers.push(name)
 }
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name)
+}
+
+
   
